17f358926fd653e7188c1579cb39dec49cf4f85e
priscilla254 <priscillawambui8@gmail.com>
2021-11-25 19:44:46 +0300

    simple mozilla webpage

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,25 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/chrome.png');
+    } else {
+      myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+  }
+   
+  myButton.onclick = function() {
+    setUserName();
+  }  
